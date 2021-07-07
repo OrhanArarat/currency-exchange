1310efedfe52e1f1ad4999439b15089457ec629f
orhanararat <orhanararat01@gmail.com>
2021-07-07 15:46:23 +0300

    Added h2 embedded db

--- src/main/java/com/orhanararat/model/CurrencyExchange.java
+++ src/main/java/com/orhanararat/model/CurrencyExchange.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "Currency_From")
     private String from;
+    @Column(name = "Currency_To")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (1000,'USD','INR','50','');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (1001,'EUR','INR','60','');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values (1002,'AUD','INR','75','');
